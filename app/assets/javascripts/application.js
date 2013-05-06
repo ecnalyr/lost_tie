32ef86132e1f09bb3f6dd12c3cad8360c5f1613d
Jason Rylance <ecnalyr@gmail.com>
2013-05-06 10:49:38 -0500

    Modified slider - changed positions and added white text shadow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,46 @@
 $(function() {
 $(document).foundation();
 });
+// Slider Controllers
+  (function(){
+      /mobile/i.test(navigator.userAgent) && !location.hash && setTimeout(function () {
+      if (!pageYOffset) window.scrollTo(0, 1);
+    }, 1000);
+      var options = {
+        nextButton: true,
+        prevButton: true,
+        animateStartingFrameIn: true,
+        transitionThreshold: 500,
+        preloader: true,
+        preloadTheseFrames: [1,2,3],
+        preloadTheseImages: [
+          "../../img/slider-imgs/bg1.jpg",
+          "../../img/slider-imgs/bg2.jpg",
+          "../../img/slider-imgs/bg3.jpg",
+          "../../img/slider-imgs/bg4.jpg"
+        ],
+        fallback: {
+                theme: "slide",
+                speed: 500
+            }
+      };
+
+      var sequence = $("#sequence").sequence(options).data("sequence"),
+          $slides = $('#sequence-slides'), counter = '' ;
+
+          if(sequence){
+
+             sequence.beforeFirstFrameAnimatesIn= function(){
+                $slides.find('.slider-bg').each(function(i){
+                  counter = ++i;
+                  $(this).addClass('header-' + counter);
+                });
+              };
+
+          }
+          else{
+            return;
+          }
+
+
+  })();
