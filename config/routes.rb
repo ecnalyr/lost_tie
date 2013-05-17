1488575971a9a53454dc0693db8d897675a021e0
Jason Rylance <ecnalyr@gmail.com>
2013-05-17 09:30:58 -0500

    Added forwarding for /contact to /contact-us

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 LostTie::Application.routes.draw do
   root :to => "home#index"
 
+  match "/contact" => redirect("/contact-us")
+
   resources :portfolio, :only => :index
   match '/portfolio' => 'portfolio#index'
 end
