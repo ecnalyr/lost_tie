6c33e1e985ae9849186de6d7096c15597f5bd0aa
Jason Rylance <ecnalyr@gmail.com>
2013-05-17 09:28:56 -0500

    Removed unecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 LostTie::Application.routes.draw do
   root :to => "home#index"
-  
-  resources :contact, :only => :index
-  match '/contact' => 'contact#index'
 
   resources :portfolio, :only => :index
   match '/portfolio' => 'portfolio#index'
